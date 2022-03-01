65e4473fab65771590657bde089f45ef026385f7
Wong King Wang <Kelvin>
2022-03-02 00:52:14 +0800

    modified files

--- js/page.js
+++ js/page.js
@@ -2,7 +2,7 @@ let user_input = {
     country : "",
     language: "",
     check_mask: false,
-    check_temp: false,
+    check_temp: true,
     check_vax: false
 }
 
@@ -38,4 +38,21 @@ function record_vax() {
         user_input.check_vax = true;
         console.log("Vax:" + user_input.check_vax);
 
+}
+
+function load_workflow() {
+    for (const key in user_input) {
+        console.log(key);
+        console.log(user_input[key]);
+        if(key == "check_mask" || key == "check_temp" || key == "check_vax") {
+            if(user_input[key] == true) {
+                console.log("true");
+                var new_div = document.createElement("div");
+                new_div.innerHTML = key;
+                new_div.setAttribute('class', 'workflow-confirm-design');
+                let section = document.getElementById("workflow-confirmation-body");
+                section.appendChild(new_div);
+            }
+        }
+    }
 }
