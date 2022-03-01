c818a0b3d9dd7b71a2062e634f188827e3fd09d7
Wong King Wang <Kelvin>
2022-03-01 22:30:01 +0800

    modified files

--- js/page.js
+++ js/page.js
@@ -1,7 +1,9 @@
 let user_input = {
     country : "",
     language: "",
-    workflow_preference : []
+    check_mask: false,
+    check_temp: false,
+    check_vax: false
 }
 
 function record_country(value) {
@@ -10,4 +12,30 @@ function record_country(value) {
 
 function record_language(value) {
     user_input.language = value;
+}
+
+function record_mask() {
+    if(user_input.check_mask)
+        user_input.check_mask = false;
+    else
+        user_input.check_mask = true;
+    console.log("Mask:" + user_input.check_mask);
+}
+
+function record_temp() {
+    if(user_input.check_temp)
+        user_input.check_temp = false;
+    else
+        user_input.check_temp = true;
+    console.log("temp:" + user_input.check_temp);
+
+}
+
+function record_vax() {
+    if(user_input.check_vax)
+        user_input.check_vax = false;
+    else
+        user_input.check_vax = true;
+        console.log("Vax:" + user_input.check_vax);
+
 }
